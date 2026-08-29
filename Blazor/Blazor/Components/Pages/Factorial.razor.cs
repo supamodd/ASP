1afb02489666ddbef683012c366c2a904d248807
supamodd <supamodd@DESKTOP-AE5N24E>
2026-08-29 13:53:55 +0300

    FACTORIAL DONE

---
+++ Blazor/Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Factorial
+	{
+		int n = 0;  //RAII - Resource Aquisition is Initialization
+					//		(Выделение ресурсов - это Инициализация)
+		BigInteger f = 1;
+		void Calculate()
+		{
+			f = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				f *= i;
+			}
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+﻿https://github.com/okovtun/SPU_411_ASP
+https://www.youtube.com/playlist?list=PLM4V0mnGe_ao
+
+TODO:
+1. В проект 'Blazor' добавить компонент 'Power', который возводит указанное число в указанную степень;
+2. В проект 'Blazor' добавить компонент 'Fibonacci', который выводит на экран заданное количество чисел из ряда Фибоначчи;
