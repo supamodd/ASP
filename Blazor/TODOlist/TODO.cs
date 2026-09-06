32d47c25428cafcc4ada89409fe241cd314a71f3
supamodd <supamodd@DESKTOP-AE5N24E>
2026-09-06 10:03:09 +0300

    upd2

---
+++ Blazor/TODOlist/TODO.cs
@@ -0,0 +1,15 @@
+﻿namespace TODOlist
+{
+	public class TODO
+	{
+		public string Description { get; set; }
+		public bool DONE { get; set; }
+		public override bool Equals(object? other)
+		{
+			return 
+				this.
+				Description.
+				Equals((other as TODO).Description, StringComparison.OrdinalIgnoreCase);
+		}
+	}
+}
