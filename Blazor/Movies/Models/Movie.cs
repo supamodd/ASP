48480cb83f9136b05e1867181b5dd9d131d601e9
supamodd <supamodd@DESKTOP-AE5N24E>
2026-09-15 12:12:10 +0300

    upd

---
+++ Blazor/Movies/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Movies.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public DateOnly ReleaseDate { get; set; }
+		public string Genre { get; set; }
+		public string? URL { get; set; }
+		public string? Poster { get; set; }
+	}
+}
