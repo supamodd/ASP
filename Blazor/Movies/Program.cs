6f581b286cd825a5f461f1163fe71a4ae08e82be
supamodd <supamodd@DESKTOP-AE5N24E>
2026-09-16 18:10:52 +0300

    json upd

--- Blazor/Movies/Program.cs
+++ Blazor/Movies/Program.cs
@@ -13,17 +13,23 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-	.AddInteractiveServerComponents();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    // Эндпоинт /ApplyDatabaseMigrations, по которому стучит кнопка "Apply Migrations"
+    // на странице ошибки БД. В шаблоне он был ошибочно включён только для Production,
+    // из-за чего в Development кнопка отдавала 404.
     app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -31,7 +37,25 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Создаём БД (если её ещё нет) и накатываем все неприменённые миграции при старте.
+// Ошибка только логируется, чтобы остальные страницы сайта продолжали работать.
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var dbFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<MoviesContext>>();
+        using var db = dbFactory.CreateDbContext();
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger("Migrations");
+        logger.LogError(ex, "Не удалось создать базу данных или применить миграции. " +
+                            "Проверьте строку подключения и выполните: dotnet ef database update");
+    }
+}
+
 app.MapRazorComponents<App>()
-	.AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode();
 
 app.Run();
